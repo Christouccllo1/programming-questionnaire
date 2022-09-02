b55947a3c89691423d0f40e4d400b96d4a2ee735
Christouccllo1 <corji1@hotmail.com>
2022-09-01 22:50:13 -0400

    created layout for home page

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let express = require("express")
-
+let expressLayouts = require("express-ejs-layouts")
 
 let app = express()
 let port = process.env.PORT || 5001
 
-app.get("/",(req,res)=> res.send("hello, this is home page"))
+app.use(expressLayouts)
+app.set("view engine",".ejs")
+app.get("/",(req,res)=> res.render("home"))
 
 
 
