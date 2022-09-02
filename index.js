0f52c32e80a6ca60de3ca200d18e7284722c7fdd
Christouccllo1 <corji1@hotmail.com>
2022-09-01 23:00:12 -0400

    created login and register layout

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ let port = process.env.PORT || 5001
 app.use(expressLayouts)
 app.set("view engine",".ejs")
 app.get("/",(req,res)=> res.render("home"))
+app.get("/login", (req,res)=> {
+    res.render("login")
+})
 
+app.get("/register", (req,res)=> {
+    res.render("register")
+})
 
 
 
