8f290b4d106fe5fd2909335d5a1006880ee2b9d6
Christouccllo1 <corji1@hotmail.com>
2022-09-02 22:32:32 -0400

    created login page

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ let port = process.env.PORT || 5001
 
 app.use(expressLayouts)
 app.set("view engine",".ejs")
+app.use(express.static(__dirname + "/static"))
+
+
+
+
 app.get("/",(req,res)=> res.render("home"))
 app.get("/login", (req,res)=> {
     res.render("login")
