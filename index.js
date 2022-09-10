b31d901c11dd9f3a1e23c9de408656b3bf038df9
Christouccllo1 <corji1@hotmail.com>
2022-09-09 21:31:31 -0400

    initialized passport.js for authentication

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ let express = require("express")
 let expressLayouts = require("express-ejs-layouts")
 const dotenv = require("dotenv")
 dotenv.config({ path: '.env' })
+const bcrypt = require('bcrypt')
+const User = require('./models/User.js')
+const passport = require('passport')
+const initializePassport = require('./passport-config')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+initializePassport(passport, User);
+
 let app = express()
 let port = process.env.PORT || 5001
 
@@ -9,6 +19,17 @@ app.use(expressLayouts)
 app.set("view engine",".ejs")
 app.use(express.static(__dirname + "/static"))
 app.use(express.urlencoded({extended:false}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride("_method"))
+
 
 const connectDb = require("./database/database.js")
 
